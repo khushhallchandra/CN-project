4852253278346015263b5ac19101fa1c2acbdbc1
khushhallchandra <kcm00000@gmail.com>
2019-11-12 15:55:11 -0500

    Added function

--- quic/app.js
+++ quic/app.js
@@ -11,75 +11,77 @@ const {
 var startTime, endTime;
 
 // ---------- Server ----------
-const server = new Server()
-server
-  .on('error', (err) => ilog.error(Object.assign(err, { class: 'server error' })))
-  .on('session', (session) => {
-    // ilog.info(session)
+function measureTime(data_txt, tls=false){
+    const server = new Server()
+    server
+    .on('error', (err) => ilog.error(Object.assign(err, { class: 'server error' })))
+    .on('session', (session) => {
+        // ilog.info(session)
 
-    session
-      .on('error', (err) => ilog.error(Object.assign(err, { class: 'server session error' })))
-      .on('stream', (stream) => {
-        // ilog.info(stream)
+        session
+        .on('error', (err) => ilog.error(Object.assign(err, { class: 'server session error' })))
+        .on('stream', (stream) => {
+            // ilog.info(stream)
 
-        stream
-          .on('error', (err) => ilog.error(Object.assign(err, { class: 'server stream error' })))
-          .on('data', (data) => {
-            ilog.info(`server stream ${stream.id} data: ${data.toString()}`)
-            stream.write(data)
-            endTime = new Date();
-            ilog.info(`Time taken: ${endTime - startTime}`)
-          })
-          .on('end', () => {
-            ilog.info(`server stream ${stream.id} ended`)
-            stream.end()
-          })
-          .on('finish', () => {
-            ilog.info(`server stream ${stream.id} finished`)
-          })
-      })
-  })
-
-server.listen(3000)
-  .then(() => {
-    ilog.info(Object.assign({ class: 'server listen' }, server.address()))
-  })
-  .catch(ilog.error)
-
-// ---------- Client ----------
-const cli = new Client()
-cli.on('error', (err) => ilog.error(Object.assign(err, { class: 'client error' })))
-
-thunk(function * () {
-  yield cli.connect(3000)
-  yield cli.ping()
-
-  const stream = cli.request()
-  stream
-    .on('error', (err) => ilog.error(Object.assign(err, { class: 'client stream error' })))
-    .on('data', (data) => {
-      ilog.info(`client stream ${stream.id} data: ${data.toString()}`)
-    })
-    .on('end', () => {
-      ilog.info(`client stream ${stream.id} ended`)
-      cli.close()
-    })
-    .on('finish', () => {
-      ilog.info(`client stream ${stream.id} finished`)
+            stream
+            .on('error', (err) => ilog.error(Object.assign(err, { class: 'server stream error' })))
+            .on('data', (data) => {
+                ilog.info(`server stream ${stream.id} data: ${data.toString()}`)
+                stream.write(data)
+                endTime = new Date();
+                ilog.info(`Time taken: ${endTime - startTime}`)
+            })
+            .on('end', () => {
+                ilog.info(`server stream ${stream.id} ended`)
+                stream.end()
+            })
+            .on('finish', () => {
+                ilog.info(`server stream ${stream.id} finished`)
+            })
+        })
     })
 
-  yield (done) => stream.write('hello, QUIC', done)
+    server.listen(3000)
+    .then(() => {
+        ilog.info(Object.assign({ class: 'server listen' }, server.address()))
+    })
+    .catch(ilog.error)
 
-  let i = 0
-  var txt = "abcd";
-  while (i <= 0) {
-    yield thunk.delay(100)
-    startTime = new Date();
-    yield (done) => stream.write(txt, done)
-    // i++;
-  }
-  stream.end()
+    // ---------- Client ----------
+    const cli = new Client()
+    cli.on('error', (err) => ilog.error(Object.assign(err, { class: 'client error' })))
 
-  yield (done) => cli.once('close', done)
-  yield server.close()
-})(ilog.error)
+    thunk(function * () {
+    yield cli.connect(3000)
+    yield cli.ping()
+
+    const stream = cli.request()
+    stream
+        .on('error', (err) => ilog.error(Object.assign(err, { class: 'client stream error' })))
+        .on('data', (data) => {
+        ilog.info(`client stream ${stream.id} data: ${data.toString()}`)
+        })
+        .on('end', () => {
+        ilog.info(`client stream ${stream.id} ended`)
+        cli.close()
+        })
+        .on('finish', () => {
+        ilog.info(`client stream ${stream.id} finished`)
+        })
+
+    yield (done) => stream.write('hello, QUIC', done)
+
+    let i = 0
+    var txt = "abcd";
+    while (i <= 0) {
+        yield thunk.delay(100)
+        startTime = new Date();
+        yield (done) => stream.write(data_txt, done)
+        // i++;
+    }
+    stream.end()
+
+    yield (done) => cli.once('close', done)
+    yield server.close()
+    })(ilog.error)
+}
