5c89135fcd3f8de5fb54601a44ee6a91608e975f
Ujjwal Arora <arora.ujjwal@gmail.com>
2019-11-12 17:43:00 -0500

    utils

--- http2/app.js
+++ http2/app.js
@@ -2,9 +2,7 @@
 const http2 = require('http2');
 const fs = require('fs');
 const ilog = require('ilog')
-
-const NS_PER_SEC = 1e9
-const MS_PER_NS = 1e6
+const utils = require('../utils.js')
 
 function measureTime(data, port=2000, tls=false) {
 
@@ -71,7 +69,7 @@ function measureTime(data, port=2000, tls=false) {
   
         console.log({
           headers: req.headers,
-          timings: getTimings(eventTimes),
+          timings: utils.getTimings(eventTimes),
         //   body: responseBody
         });
     });
@@ -93,28 +91,7 @@ function measureTime(data, port=2000, tls=false) {
 
 }
 
-function getHrTimeDurationInMs (startTime, endTime) {
-    const secondDiff = endTime[0] - startTime[0]
-    const nanoSecondDiff = endTime[1] - startTime[1]
-    const diffInNanoSecond = secondDiff * NS_PER_SEC + nanoSecondDiff
-  
-    return diffInNanoSecond / MS_PER_NS
-}
 
-function getTimings(eventTimes) {
-    return {
-      // There is no DNS lookup with IP address
-    //   dnsLookup: eventTimes.dnsLookupAt !== undefined ?
-    //     getHrTimeDurationInMs(eventTimes.startAt, eventTimes.dnsLookupAt) : undefined,
-    //   tcpConnection: getHrTimeDurationInMs(eventTimes.dnsLookupAt || eventTimes.startAt, eventTimes.tcpConnectionAt),
-      // There is no TLS handshake without https
-    //   tlsHandshake: eventTimes.tlsHandshakeAt !== undefined ?
-        // (getHrTimeDurationInMs(eventTimes.tcpConnectionAt, eventTimes.tlsHandshakeAt)) : undefined,
-    //   firstByte: getHrTimeDurationInMs((eventTimes.tlsHandshakeAt || eventTimes.tcpConnectionAt), eventTimes.firstByteAt),
-      contentTransfer: getHrTimeDurationInMs(eventTimes.firstByteAt, eventTimes.endAt),
-      total: getHrTimeDurationInMs(eventTimes.startAt, eventTimes.endAt)
-    }
-  }
 
 module.exports = {
     measureTime,
---
+++ utils.js
@@ -0,0 +1,29 @@
+const NS_PER_SEC = 1e9
+const MS_PER_NS = 1e6
+
+function getHrTimeDurationInMs (startTime, endTime) {
+    const secondDiff = endTime[0] - startTime[0]
+    const nanoSecondDiff = endTime[1] - startTime[1]
+    const diffInNanoSecond = secondDiff * NS_PER_SEC + nanoSecondDiff
+  
+    return diffInNanoSecond / MS_PER_NS
+}
+
+function getTimings(eventTimes) {
+    return {
+      // There is no DNS lookup with IP address
+    //   dnsLookup: eventTimes.dnsLookupAt !== undefined ?
+    //     getHrTimeDurationInMs(eventTimes.startAt, eventTimes.dnsLookupAt) : undefined,
+    //   tcpConnection: getHrTimeDurationInMs(eventTimes.dnsLookupAt || eventTimes.startAt, eventTimes.tcpConnectionAt),
+      // There is no TLS handshake without https
+    //   tlsHandshake: eventTimes.tlsHandshakeAt !== undefined ?
+        // (getHrTimeDurationInMs(eventTimes.tcpConnectionAt, eventTimes.tlsHandshakeAt)) : undefined,
+    //   firstByte: getHrTimeDurationInMs((eventTimes.tlsHandshakeAt || eventTimes.tcpConnectionAt), eventTimes.firstByteAt),
+      contentTransfer: getHrTimeDurationInMs(eventTimes.firstByteAt, eventTimes.endAt),
+      total: getHrTimeDurationInMs(eventTimes.startAt, eventTimes.endAt)
+    }
+}
+
+module.exports = {
+    getTimings,
+};
