d38cd00649bda7983058394cea68e478f8b6bbbf
mehtanihar <niharmehta79@gmail.com>
2019-11-25 01:12:06 -0500

    TLS Analysis

--- benchmark.js
+++ benchmark.js
@@ -6,8 +6,8 @@ const http1 = require('./http1.1/app.js');
 const thunk = require('thunks').thunk
 
 // BENCHMARK SIZE
-let i = 7000;
 function measureTimeForSizes(proto_type) {
+    let i = 7000;
     for(var key in dataChunks) {
         for(var j=0; j<50; j++) {
             proto_type.measureTime(key, dataChunks[key], i++);
@@ -15,14 +15,34 @@ function measureTimeForSizes(proto_type) {
     }   
 }
 
-// BENCHMARK NUMBER
+// BENCHMARK SIZE - TLS
+function measureTimeForSizesTLS(proto_type) {
+    let i = 7000;
+    for(var key in dataChunks) {
+        for(var j=0; j<50; j++) {
+            proto_type.measureTime("tls_" + key, dataChunks[key], i++, true);
+        }
+    }   
+}
 
+//1,5,25,50,100,1000
+
+// BENCHMARK NUMBER
+// function measureTimeForNumbers(proto_type) {
+//     let i = 8000;
+//     data = dataChunks["txt3"]
+//     for(var key in number_dataChunks) {
+//         for(var j=0; j<50; j++) {
+//             proto_type.measureTime("", data, k++);
+//         }
+//     }   
+// }
 
 
 // Call scripts
-measureTimeForSizes(quic);
-// measureTimeForSizes(http1);
-// measureTimeForSizes(http2);
+// measureTimeForSizes(quic);
+// measureTimeForSizesTLS(http1);
+measureTimeForSizesTLS(http2);
 // thunk.delay(100000);
 
 // measureTimeForSizes();
